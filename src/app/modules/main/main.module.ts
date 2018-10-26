8bfaa15aeabbc005bc6b71580ad9f82d94afe831
HANNACHI Ahmed <ahmedhannachi.it@gmail.com>
2018-10-26 19:15:32 +0100

    add date pipe & pokemonTypeColor pipe

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MainComponent }  from './main.component';
 
 import {BorderCardDirective} from "./directives/border-card.directive";
+import { PokemonTypeColorPipe} from "./pipes/pokemon-type-color.pipe";
 
 @NgModule({
     imports:      [ BrowserModule ],
-    declarations: [ MainComponent,BorderCardDirective ],
+    declarations: [ MainComponent,BorderCardDirective,PokemonTypeColorPipe ],
     bootstrap:    [ MainComponent ]
 })
 export class MainModule { }
